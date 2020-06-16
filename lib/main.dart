0603f1dbc7416eee24a1279a3b8fa63bf0139230
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-06-16 11:00:03 +0530

    UI update

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,24 @@
 import 'package:flutter/material.dart';
+import './bloc/fruit_bloc.dart';
+import './pages/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter SEMBAST',
-    
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
- 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter SEMBAST'),
-      ),
-      body: Center(
-        child: Text("SEMBAST DEMO"),
+    // Wrapping the whole app with BlocProvider to get access to FruitBloc everywhere
+    // BlocProvider extends InheritedWidget.
+    return BlocProvider(
+      bloc: FruitBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          accentColor: Colors.redAccent,
+        ),
+        home: HomePage(),
       ),
     );
   }
